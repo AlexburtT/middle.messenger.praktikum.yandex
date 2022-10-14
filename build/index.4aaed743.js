fefecc2d4425cb1c5b68906d023c823eb80c8f66
NeoSmait <neosmain@ya.ru>
2022-10-14 23:06:50 +0900

    build

---
+++ build/index.4aaed743.js
@@ -0,0 +1,2 @@
+document.querySelector("#root").textContent=function(...t){if(0===t.length)throw Error("sum required at least 1 argument");return t.reduce(((t,e)=>t+e),0)}(100,-1).toString();
+//# sourceMappingURL=index.4aaed743.js.map
