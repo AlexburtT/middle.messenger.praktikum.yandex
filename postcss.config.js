4abb84e0252b6945ac42cc13465ab65a0567184b
NeoSmait <neosmain@ya.ru>
2022-10-15 23:06:15 +0900

    pages

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	plugins: [
+		require('autoprefixer'),
+		require('postcss-nested'),
+		require('postcss-import'),
+		require('postcss-url')
+	]
+}
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import { sum } from './modules/sum';
-
-const root = document.querySelector('#root');
-root.textContent = sum(100, -1).toString(); 
